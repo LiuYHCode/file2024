4dbd944bd37667843a95a74672850c8ace007583
LiuYHCode <lyh997835@163.com>
2024-09-19 23:17:53 +0800

    初始项目框架搭建

---
+++ myIo/src/main/java/com/at2024/Main.java
@@ -0,0 +1,11 @@
+package com.at2024;
+
+/**
+ * @author lyh
+ * @date 2024-09-19 22:38:33
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
